bdabbd2dea030577f9d11316f49a638a5a667675
Mina Lee <minaleedev@gmail.com>
2021-07-29 19:07:50 +0100

    🚧  Import tiny-slider.js & initialise productSlider

---
+++ js/productSlider.js
@@ -0,0 +1,9 @@
+const productSlider = tns({
+  container: '.product-slider .slider-list',
+  controls: false,
+  navContainer: '.product-slider .thumbnail-list',
+  navAsThumbnails: true,
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+})
